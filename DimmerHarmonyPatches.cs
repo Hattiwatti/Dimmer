ec64c58a598db327beee90e6c6412611058f3e27
Hattiwatti <hattiwatti05@gmail.com>
2025-08-23 15:56:25 +0200

    Fix TubeBloomPrePassLight not being dimmed

--- DimmerHarmonyPatches.cs
+++ DimmerHarmonyPatches.cs
@@ -27,47 +27,13 @@ namespace Dimmer
             }
         }
 
-        [AffinityPrefix]
-        [AffinityPatch(typeof(BloomPrePassBackgroundLightWithId), "ColorWasSet")]
-        [AffinityPatch(typeof(InstancedMaterialLightWithId), "ColorWasSet")]
-        [AffinityPatch(typeof(LightWithIds.LightWithId), "ColorWasSet")]
-        private void DimNewColor(ref ILightWithId __instance, ref Color newColor)
+        private void DimColor(ref Color color)
         {
-            DimColor(ref __instance, ref newColor);
-        }
-
-        [AffinityPrefix]
-        [AffinityPatch(typeof(BeatLine), "ColorWasSet")]
-        [AffinityPatch(typeof(BloomPrePassBackgroundColorsGradientElementWithLightId), "ColorWasSet")]
-        [AffinityPatch(typeof(BloomPrePassBackgroundColorsGradientTintColorWithLightId), "ColorWasSet")]
-        [AffinityPatch(typeof(BloomPrePassBackgroundColorsGradientTintColorWithLightIds), "ColorWasSet")]
-        [AffinityPatch(typeof(DirectionalLightWithId), "ColorWasSet")]
-        [AffinityPatch(typeof(DirectionalLightWithIds), "ColorWasSet")]
-        [AffinityPatch(typeof(DirectionalLightWithLightGroupIds), "ColorWasSet")]
-        [AffinityPatch(typeof(EnableRendererWithLightId), "ColorWasSet")]
-        [AffinityPatch(typeof(MaterialLightWithId), "ColorWasSet")]
-        [AffinityPatch(typeof(MaterialLightWithIds), "ColorWasSet")]
-        [AffinityPatch(typeof(ParticleSystemLightWithId), "ColorWasSet")]
-        [AffinityPatch(typeof(ParticleSystemLightWithIds), "ColorWasSet")]
-        [AffinityPatch(typeof(PointLightWithIds), "ColorWasSet")]
-        [AffinityPatch(typeof(RectangleFakeGlowLightWithId), "ColorWasSet")]
-        [AffinityPatch(typeof(SongTimeSyncedVideoPlayer), "ColorWasSet")]
-        [AffinityPatch(typeof(SpawnRotationChevron), "ColorWasSet")]
-        [AffinityPatch(typeof(SpriteLightWithId), "ColorWasSet")]
-        [AffinityPatch(typeof(TubeBloomPrePassLightWithId), "ColorWasSet")]
-        [AffinityPatch(typeof(UnityLightWithId), "ColorWasSet")]
-        private void DimColor(ref ILightWithId __instance, ref Color color)
-        {
-            // Don't dim the feet
-            if (__instance == _feetMarker)
-            {
-                return;
-            }
-
             if (!_config.DimmerEnabled || !Plugin.IsPlayingChart)
             {
                 return;
             }
+
             switch (_config.Mode)
             {
                 case DimmerMode.Multiplier:
@@ -99,6 +65,61 @@ namespace Dimmer
                     }
                     break;
             }
+        }
+
+        [AffinityPrefix]
+        [AffinityPatch(typeof(BloomPrePassBackgroundLightWithId), "ColorWasSet")]
+        [AffinityPatch(typeof(InstancedMaterialLightWithId), "ColorWasSet")]
+        [AffinityPatch(typeof(LightWithIds.LightWithId), "ColorWasSet")]
+        [AffinityPatch(typeof(ColorArrayLightWithIds.ColorArrayLightWithId), "ColorWasSet")]
+        private void ColorWasSetNew(ref ILightWithId __instance, ref Color newColor)
+        {
+            DimColor(ref newColor);
+        }
+
+        [AffinityPrefix]
+        [AffinityPatch(typeof(BeatLine), "ColorWasSet")]
+        [AffinityPatch(typeof(BloomPrePassBackgroundColorsGradientElementWithLightId), "ColorWasSet")]
+        [AffinityPatch(typeof(BloomPrePassBackgroundColorsGradientTintColorWithLightId), "ColorWasSet")]
+        [AffinityPatch(typeof(BloomPrePassBackgroundColorsGradientTintColorWithLightIds), "ColorWasSet")]
+        [AffinityPatch(typeof(DirectionalLightWithId), "ColorWasSet")]
+        [AffinityPatch(typeof(DirectionalLightWithIds), "ColorWasSet")]
+        [AffinityPatch(typeof(DirectionalLightWithLightGroupIds), "ColorWasSet")]
+        [AffinityPatch(typeof(EnableRendererWithLightId), "ColorWasSet")]
+        [AffinityPatch(typeof(MaterialLightWithId), "ColorWasSet")]
+        [AffinityPatch(typeof(MaterialLightWithIds), "ColorWasSet")]
+        [AffinityPatch(typeof(ParticleSystemLightWithId), "ColorWasSet")]
+        [AffinityPatch(typeof(ParticleSystemLightWithIds), "ColorWasSet")]
+        [AffinityPatch(typeof(PointLightWithIds), "ColorWasSet")]
+        [AffinityPatch(typeof(RectangleFakeGlowLightWithId), "ColorWasSet")]
+        [AffinityPatch(typeof(SongTimeSyncedVideoPlayer), "ColorWasSet")]
+        [AffinityPatch(typeof(SpawnRotationChevron), "ColorWasSet")]
+        [AffinityPatch(typeof(SpriteLightWithId), "ColorWasSet")]
+        [AffinityPatch(typeof(TubeBloomPrePassLightWithId), "ColorWasSet")]
+        [AffinityPatch(typeof(UnityLightWithId), "ColorWasSet")]
+        private void ColorWasSet(ref ILightWithId __instance, ref Color color)
+        {
+            // Don't dim the feet
+            if (__instance == _feetMarker)
+            {
+                return;
+            }
+
+            DimColor(ref color);
+        }
+        
+        [AffinityPrefix]
+        [AffinityPatch(typeof(ParametricBoxController), "Refresh")]
+        private void Refresh(ref ParametricBoxController __instance)
+        {
+            //DimColor(ref __instance.color);
+        }
+
+        [AffinityPrefix]
+        [AffinityPatch(typeof(TubeBloomPrePassLight), nameof(TubeBloomPrePassLight.color), AffinityMethodType.Setter)]
+        private void SetColor(ref Color value)
+        {
+            DimColor(ref value);
         }
     }
 
